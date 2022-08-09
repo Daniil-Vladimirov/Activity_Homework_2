b242394323e6727587c90b35290437aa86e6f33f
Daniil <porvipop@mail.ru>
2022-08-09 12:58:26 +0300

    Add send message from activity1 to activity2 functionality

--- app/src/main/java/com/example/activity_homework_2/MainActivity.kt
+++ app/src/main/java/com/example/activity_homework_2/MainActivity.kt
@@ -5,18 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 
 const val TAG = "MainActivity"
+const val EXTRA_MESSAGE = "com.example.activity_homework_2.extra.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
+
+    private var editTextMessage: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        editTextMessage = findViewById<EditText>(R.id.ed_text_main_activity)
     }
 
     fun launchSecondActivity(view: View) {
+        val message = editTextMessage?.text.toString()
         val intent = Intent(this, SecondActivity::class.java)
+            .putExtra(EXTRA_MESSAGE, message)
         Log.d(TAG, "Button clicked")
         startActivity(intent)
     }
--- app/src/main/java/com/example/activity_homework_2/SecondActivity.kt
+++ app/src/main/java/com/example/activity_homework_2/SecondActivity.kt
@@ -2,10 +2,15 @@ package com.example.activity_homework_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        val textView = findViewById<TextView>(R.id.textView_message)
+        textView.text = message
     }
 }
