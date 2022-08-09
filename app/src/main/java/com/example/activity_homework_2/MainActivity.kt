4e8caaae6d31e321babdab26e8c088649ef4ea42
Daniil <porvipop@mail.ru>
2022-08-09 14:38:45 +0300

    Add startActivityForResult from Second activity

--- app/src/main/java/com/example/activity_homework_2/MainActivity.kt
+++ app/src/main/java/com/example/activity_homework_2/MainActivity.kt
@@ -6,25 +6,45 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
+import androidx.core.view.isVisible
 
 const val TAG = "MainActivity"
 const val EXTRA_MESSAGE = "com.example.activity_homework_2.extra.MESSAGE"
+const val TEXT_REQUEST = 1
 
 class MainActivity : AppCompatActivity() {
 
     private var editTextMessage: EditText? = null
+    private var replyHeaderTextView: TextView? = null
+    private var replyText: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        editTextMessage = findViewById<EditText>(R.id.ed_text_main_activity)
+        editTextMessage = findViewById(R.id.ed_text_main_activity)
+        replyHeaderTextView = findViewById(R.id.header_reply)
+        replyText = findViewById(R.id.textView_reply_message)
     }
 
     fun launchSecondActivity(view: View) {
+        Log.d(TAG, "Button clicked")
         val message = editTextMessage?.text.toString()
         val intent = Intent(this, SecondActivity::class.java)
             .putExtra(EXTRA_MESSAGE, message)
-        Log.d(TAG, "Button clicked")
-        startActivity(intent)
+        startActivityForResult(intent, TEXT_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                replyHeaderTextView?.visibility = View.VISIBLE
+
+                val reply = data?.getStringExtra(EXTRA_REPLY)
+                replyText?.text = reply
+                replyText?.visibility = View.VISIBLE
+            }
+        }
     }
 }
--- app/src/main/java/com/example/activity_homework_2/SecondActivity.kt
+++ app/src/main/java/com/example/activity_homework_2/SecondActivity.kt
@@ -1,16 +1,35 @@
 package com.example.activity_homework_2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 
+const val EXTRA_REPLY = "com.example.activity_homework_2.extra.REPLY"
+
 class SecondActivity : AppCompatActivity() {
+
+    private var replyTextMessage: EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        replyTextMessage = findViewById(R.id.ed_text_second_activity)
         val message = intent.getStringExtra(EXTRA_MESSAGE)
-
         val textView = findViewById<TextView>(R.id.textView_message)
         textView.text = message
+    }
+
+    fun returnReply(view: View) {
+        Log.d(TAG, "Reply clicked")
+        val replyMessage = replyTextMessage?.text.toString()
+        val intent = Intent()
+            .putExtra(EXTRA_REPLY, replyMessage)
+        setResult(RESULT_OK, intent)
+        finish()
+
     }
 }
