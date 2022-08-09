5fb607ac75b4ae991bd08ca91c2a6e33e9caf9b5
Daniil <porvipop@mail.ru>
2022-08-09 12:22:47 +0300

    Create second_activity design

--- app/src/main/java/com/example/activity_homework_2/MainActivity.kt
+++ app/src/main/java/com/example/activity_homework_2/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.activity_homework_2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+
+const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun launchSecondActivity(view: View) {
+        val intent = Intent(this, SecondActivity::class.java)
+        Log.d(TAG, "Button clicked")
+        startActivity(intent)
     }
 }
